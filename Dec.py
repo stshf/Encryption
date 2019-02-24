8b0ec28e9c5b25d49aa098fd046856e975c368ca
Satoshi Fukui <kobesatoshi@outlook.jp>
2019-02-24 22:50:15 +0900

    Dec

---
+++ Dec.py
@@ -0,0 +1,45 @@
+def Dec(c, n):
+    """input
+        c : chiper text 
+        type char
+
+        n : key 1 ~ 25
+        type int
+        
+        output
+        m : plain text
+        type char
+    """
+    m = ""
+    for c_ in c:
+        ascii_c_ = ord(c_)
+        if ord('a') <= ascii_c_ <= ord('z'):
+            m += chr((ord(c_) - ord('a') - n) % 26  + ord('a'))
+        elif ord('A') <= ascii_c_ <= ord('Z'):
+            m += chr((ord(c_) - ord('A') - n) % 26 + ord('A'))
+        else:
+            m += c_
+    return m
+
+
+
+
+def main():
+    print("input chiper text (Alpahbet)")
+    Chiper_text_list = list(input().split())
+    print("")
+
+    print("input key")
+    key = int(input())
+    print("")
+
+    Plain_text_list = []
+    for Chiper_text in Chiper_text_list:
+        Plain_text_list.append(Dec(Chiper_text, key))
+        
+    print("Plain text")
+    print(" ".join(Plain_text_list))
+    
+
+if __name__ == "__main__":
+    main()
